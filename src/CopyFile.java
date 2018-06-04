156c4d6cf0afc964defbf04f8bbc08622e47138d
crismar24 <crismar24@gmail.com>
2018-06-04 20:55:44 +0300

    copy file

--- src/CopyFile.java
+++ src/CopyFile.java
@@ -12,33 +12,42 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
-
-import static java.lang.System.out;
+import java.util.Date;
 
 
 public class CopyFile {
 
-    void copy(String line) {
-        //System.out.println("Not correct format of a way to files");
+    public static void main(String[] args) {
+        args = new String[2];
+        args[0] = "E:\\Downloads\\Gringo.2018.WEB-DLRip.1.46Gb.MegaPeer.avi";
+        args[1] = "E:\\Downloads\\Gringo.2018.WEB-DLRip.1.46Gb.MegaPeer-copy111.avi";
 
-        public static void main (String args[]) {
+        try {
+            Date dateStart = new Date();
             copyFiles(new File(args[0]), new File(args[1]));
-        }
-
-        public static void copyFiles(File in, File out) throws IOException {
-            FileChannel inChannel = new
-                    FileInputStream(in).getChannel();
-            FileChannel outChannel = new
-                    FileOutputStream(out).getChannel();
-            try {
-                inChannel.transferTo(0, inChannel.size(),
-                        outChannel);
-            } catch (IOException e) {
-                throw e;
-            } finally {
-                if (inChannel != null) inChannel.close();
-                if (outChannel != null) outChannel.close();
-            }
+            Date dateEnd = new Date();
+            System.out.println("Время, затраченнное на копирование файла: " + (dateEnd.getTime() - dateStart.getTime()) + " милисекунд");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println(e);
+            // TODO описание ошибки ?
         }
     }
+
+    public static void copyFiles(File in, File out) throws IOException {
+        FileChannel inChannel = new
+                FileInputStream(in).getChannel();
+        FileChannel outChannel = new
+                FileOutputStream(out).getChannel();
+        try {
+            inChannel.transferTo(0, inChannel.size(),
+                    outChannel);
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            if (inChannel != null) inChannel.close();
+            if (outChannel != null) outChannel.close();
+        }
+    }
+}
 
